9dcb14f320c70482dade4e2d49259e692d0b8c47
Natanael Corpas Rivero <natel1988@gmail.com>
2025-05-05 18:07:38 +0200

    actualizacion lunes 18:07

--- js/donuts.js
+++ js/donuts.js
@@ -24,6 +24,7 @@ class Donut {
         this.node.style.left = `${this.x}px`;
 
 
+
     }
 
     //metodos
--- js/main.js
+++ js/main.js
@@ -5,22 +5,25 @@ const splashScreenNode = document.querySelector("#splash-screen");
 const gameScreenNode = document.querySelector("#game-screen");
 const gameOverScreenNode = document.querySelector("#game-over.screen");
 const gameBoxNode = document.querySelector("#game-box");
+const scoreBoard = document.querySelector("#score");
 
 // Botones
 const startBtnNode = document.querySelector("#start-btn");
 
 // Game box
 
-
 // VARIABLES GLOBALES DEL JUEGO
 let homerObj = null;
 let donutObjArr = [];
 let frutaObjArr = [];
+let gameIntervalId = null;
+let donutIntervalId = null;
+let frutaIntervalId = null;
+let score = 3;
 
 // FUNCIONES GLOBALES DEL JUEGO
 
 function startGame() {
-
   // 1. Ocultar pantalla de inicio
   splashScreenNode.style.display = "none";
 
@@ -28,21 +31,21 @@ function startGame() {
   gameScreenNode.style.display = "flex";
 
   // 3. añadimos elemento inicial del juego (Homer)
-  
+
   homerObj = new Homer();
   // 4. iniciamos intervalo principal del juego
-  setInterval(() => {
+  gameIntervalId = setInterval(() => {
     gameLoop();
   }, Math.round(1000 / 60)); //60fps
 
   // 5. iniciamos otro intervalo del juego
-  setInterval(() => {
+  donutIntervalId = setInterval(() => {
     donutAppear();
-  },2000) //los donuts aparecen cada dos segundos
+  }, 2000); //los donuts aparecen cada dos segundos
 
-  setInterval(() => {
+  frutaIntervalId = setInterval(() => {
     frutaAppear();
-  }, 1000)
+  }, 1000);
 }
 
 function gameLoop() {
@@ -55,15 +58,16 @@ function gameLoop() {
   });
 
   elementsDestoy();
-
+  colisionHomerDonut();
+  colisionHomerFruit();
 }
 
-function donutAppear(){
+function donutAppear() {
   let donutObj = new Donut(Math.random() * 370);
   donutObjArr.push(donutObj);
 }
 
-function frutaAppear(){
+function frutaAppear() {
   let frutaObj1 = new Fruta("cereza", Math.random() * 370);
   frutaObjArr.push(frutaObj1);
 
@@ -71,18 +75,62 @@ function frutaAppear(){
   frutaObjArr.push(frutaObj2);
 }
 
-function elementsDestoy(){
-
-  if(donutObjArr.length < 730 && (donutObjArr[0].y + donutObjArr[0].h) >= 730){
+function elementsDestoy() {
+  if (donutObjArr.length < 730 && donutObjArr[0].y + donutObjArr[0].h >= 730) {
     donutObjArr[0].node.remove();
     donutObjArr.shift();
   }
 
-  if(frutaObjArr.length < 730 && (frutaObjArr[0].y + frutaObjArr[0].h) >= 730){
+  if (frutaObjArr.length < 730 && frutaObjArr[0].y + frutaObjArr[0].h >= 730) {
     frutaObjArr[0].node.remove();
     frutaObjArr.shift();
   }
+}
 
+function colisionHomerDonut() {
+  donutObjArr.forEach((eachDonutObj) => {
+    if (
+      homerObj.x < eachDonutObj.x + eachDonutObj.w &&
+      homerObj.x + homerObj.w > eachDonutObj.x &&
+      homerObj.y < eachDonutObj.y + eachDonutObj.h &&
+      homerObj.h + homerObj.y > eachDonutObj.y
+    ) {
+      // ¡colisión detectada!
+      score++;
+      scoreBoard.textContent = `Score: ${score}`;
+      donutObjArr[0].node.remove();
+      donutObjArr.shift();
+    }
+  });
+}
+
+function colisionHomerFruit() {
+  frutaObjArr.forEach((eachFrutaObj) => {
+    if (
+      homerObj.x < eachFrutaObj.x + eachFrutaObj.w &&
+      homerObj.x + homerObj.w > eachFrutaObj.x &&
+      homerObj.y < eachFrutaObj.y + eachFrutaObj.h &&
+      homerObj.h + homerObj.y > eachFrutaObj.y
+    ) {
+      // ¡colisión detectada!
+      score--;
+      scoreBoard.textContent = `Score: ${score}`;
+      frutaObjArr[0].node.remove();
+      frutaObjArr.shift();
+    }
+  });
+}
+
+function dameOver() {
+  //1. detener el intervalo
+  clearInterval(gameIntervalId);
+  clearInterval(donutIntervalId);
+  clearInterval(frutaIntervalId);
+  //2. ocultar pantalla de juego
+  gameScreenNode.style.display = "none";
+
+  //3 mostrar pantalla final
+  gameOverScreenNode.style.display = "flex";
 }
 
 // EVENT LISTENERS
